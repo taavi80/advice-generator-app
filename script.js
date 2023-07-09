7d3d7ac93cbb388ed1815c7f9959833da75208c5
taavi80 <123880364+taavi80@users.noreply.github.com>
2023-07-09 18:42:38 +0300

    updated index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const diceImage = document.querySelector(".dice-wrapper");
+
+diceImage.addEventListener("click", fetchAdvice);
+
+window.addEventListener("load", fetchAdvice);
+
+async function fetchAdvice() {
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    const advice = data.slip.advice;
+
+    const adviceId = document.getElementById("adviceId");
+    const adviceText = document.getElementById("adviceText");
+    adviceId.textContent = `Advice #${data.slip.id}`;
+    adviceText.textContent = advice;
+  } catch (error) {
+    console.log("Error fetching advice:", error);
+  }
+}
